23593ff98f72c9d89c3dfa665ce1a0f771e74abd
22-0033c <22-0033c@CCL1-01A>
2024-03-11 17:24:56 +0800

    Added design, timer, rules, and leaderboard left

--- PrelimWPF/Game.xaml.cs
+++ PrelimWPF/Game.xaml.cs
@@ -24,7 +24,129 @@ namespace PrelimWPF
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Btn1_Click(object sender, RoutedEventArgs e)
+        {
+            if (btn1.Content.ToString() == "0")
+            {
+                btn1.Content = "1";
+                Apple8.Source = new BitmapImage(new Uri("/Golden_Apple_JE2_BE2-removebg-preview.png", UriKind.RelativeOrAbsolute));
+            }
+            else
+            {
+                btn1.Content = "0";
+                Apple8.Source = new BitmapImage(new Uri("/minecraft_apple-removebg-preview.png", UriKind.RelativeOrAbsolute));
+            }
+        }
+
+        private void Btn2_Click(object sender, RoutedEventArgs e)
+        {
+            if (btn2.Content.ToString() == "0")
+            {
+                btn2.Content = "1";
+                Apple7.Source = new BitmapImage(new Uri("/Golden_Apple_JE2_BE2-removebg-preview.png", UriKind.RelativeOrAbsolute));
+            }
+            else
+            {
+                btn2.Content = "0";
+                Apple7.Source = new BitmapImage(new Uri("/minecraft_apple-removebg-preview.png", UriKind.RelativeOrAbsolute));
+            }
+        }
+
+        private void Btn4_Click(object sender, RoutedEventArgs e)
+        {
+            if (btn4.Content.ToString() == "0")
+            {
+                btn4.Content = "1";
+                Apple6.Source = new BitmapImage(new Uri("/Golden_Apple_JE2_BE2-removebg-preview.png", UriKind.RelativeOrAbsolute));
+            }
+            else
+            {
+                btn4.Content = "0";
+                Apple6.Source = new BitmapImage(new Uri("/minecraft_apple-removebg-preview.png", UriKind.RelativeOrAbsolute));
+            }
+        }
+
+        private void Btn8_Click(object sender, RoutedEventArgs e)
+        {
+            if (btn8.Content.ToString() == "0")
+            {
+                btn8.Content = "1";
+                Apple5.Source = new BitmapImage(new Uri("/Golden_Apple_JE2_BE2-removebg-preview.png", UriKind.RelativeOrAbsolute));
+            }
+            else
+            {
+                btn8.Content = "0";
+                Apple5.Source = new BitmapImage(new Uri("/minecraft_apple-removebg-preview.png", UriKind.RelativeOrAbsolute));
+            }
+        }
+
+        private void Btn16_Click(object sender, RoutedEventArgs e)
+        {
+            if (btn16.Content.ToString() == "0")
+            {
+                btn16.Content = "1";
+                Apple4.Source = new BitmapImage(new Uri("/Golden_Apple_JE2_BE2-removebg-preview.png", UriKind.RelativeOrAbsolute));
+            }
+            else
+            {
+                btn16.Content = "0";
+                Apple4.Source = new BitmapImage(new Uri("/minecraft_apple-removebg-preview.png", UriKind.RelativeOrAbsolute));
+            }
+        }
+
+        private void Btn32_Click(object sender, RoutedEventArgs e)
+        {
+            if (btn32.Content.ToString() == "0")
+            {
+                btn32.Content = "1";
+                Apple3.Source = new BitmapImage(new Uri("/Golden_Apple_JE2_BE2-removebg-preview.png", UriKind.RelativeOrAbsolute));
+            }
+            else
+            {
+                btn32.Content = "0";
+                Apple3.Source = new BitmapImage(new Uri("/minecraft_apple-removebg-preview.png", UriKind.RelativeOrAbsolute));
+            }
+        }
+
+        private void Btn64_Click(object sender, RoutedEventArgs e)
+        {
+            if (btn64.Content.ToString() == "0")
+            {
+                btn64.Content = "1";
+                Apple2.Source = new BitmapImage(new Uri("/Golden_Apple_JE2_BE2-removebg-preview.png", UriKind.RelativeOrAbsolute));
+            }
+            else
+            {
+                btn64.Content = "0";
+                Apple2.Source = new BitmapImage(new Uri("/minecraft_apple-removebg-preview.png", UriKind.RelativeOrAbsolute));
+            }
+        }
+
+        private void Btn128_Click(object sender, RoutedEventArgs e)
+        {
+            if (btn128.Content.ToString() == "0")
+            {
+                btn128.Content = "1";
+                Apple.Source = new BitmapImage(new Uri("/Golden_Apple_JE2_BE2-removebg-preview.png", UriKind.RelativeOrAbsolute));
+            }
+            else
+            {
+                btn128.Content = "0";
+                Apple.Source = new BitmapImage(new Uri("/minecraft_apple-removebg-preview.png", UriKind.RelativeOrAbsolute));
+            }
+        }
+
+        private void BtnStart_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnFinish_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnExit_Click(object sender, RoutedEventArgs e)
         {
 
         }
